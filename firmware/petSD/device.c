b8c817c303d969f206fd2b0e873babf828e91c12
Nils Eilers <nils.eilers@gmx.de>
2012-10-07 14:41:44 +0200

    Fixed: didn't compile if device hasn't IEC bus
    Makefile always includes iec.c no matter if the device has an IEC bus.
    iec.c wants to include <device>/iechw.h that might not be there --
    compilation stops.
    
    Added #ifdef HAS_IEC / #ifdef HAS_IEEE to switch iec.c / ieee.c off if
    not needed.

--- firmware/petSD/device.c
+++ firmware/petSD/device.c
@@ -1,8 +1,9 @@
 /*
     XD-2031 - Serial line filesystem server for CBMs 
     Copyright (C) 2012  Andre Fachat (afachat@gmx.de)
+    Copyright (C) 2012 Nils Eilers (nils.eilers@gmx.de)
 
-    XS-1541 specific device initialization
+    petSD specific device initialization
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
@@ -25,16 +26,9 @@
 
 void device_init(void) {
 
-#ifdef HAS_IEEE
         // IEEE488 bus
         ieeehw_init();                  // hardware
         ieee_init(8);                   // hardware-independent part; registers as bus
-#endif
-#ifdef HAS_IEC
-        // IEC bus
-        iechw_init();                   // hardware
-        iec_init(8);                    // hardware-independent part; registers as bus
-#endif
 
 }
 
