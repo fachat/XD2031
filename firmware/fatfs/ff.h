a03c09f58e45951968c8559aa47f73270a8e2c3e
Nils Eilers <nils.eilers@gmx.de>
2012-11-15 12:34:53 +0100

    Map FatFs error codes to XD-2031 ones

--- firmware/fatfs/ff.h
+++ firmware/fatfs/ff.h
@@ -21,6 +21,7 @@
 extern "C" {
 #endif
 
+#include "../errors.h"	/* XD-2031 error codes */
 #include "integer.h"    /* Basic integer types */
 #include "ffconf.h"     /* FatFs configuration options */
 
@@ -63,7 +64,7 @@ typedef WCHAR TCHAR;
 
 #else                       /* ANSI/OEM string */
 #ifndef _INC_TCHAR
-typedef char TCHAR;
+typedef unsigned char TCHAR;
 #define _T(x) x
 #define _TEXT(x) x
 #endif
@@ -173,28 +174,29 @@ typedef struct {
 
 
 /* File function return code (FRESULT) */
+/* TODO: improve mapping */
 
 typedef enum {
-    FR_OK = 0,              /* (0) Succeeded */
-    FR_DISK_ERR,            /* (1) A hard error occurred in the low level disk I/O layer */
-    FR_INT_ERR,             /* (2) Assertion failed */
-    FR_NOT_READY,           /* (3) The physical drive cannot work */
-    FR_NO_FILE,             /* (4) Could not find the file */
-    FR_NO_PATH,             /* (5) Could not find the path */
-    FR_INVALID_NAME,        /* (6) The path name format is invalid */
-    FR_DENIED,              /* (7) Access denied due to prohibited access or directory full */
-    FR_EXIST,               /* (8) Access denied due to prohibited access */
-    FR_INVALID_OBJECT,      /* (9) The file/directory object is invalid */
-    FR_WRITE_PROTECTED,     /* (10) The physical drive is write protected */
-    FR_INVALID_DRIVE,       /* (11) The logical drive number is invalid */
-    FR_NOT_ENABLED,         /* (12) The volume has no work area */
-    FR_NO_FILESYSTEM,       /* (13) There is no valid FAT volume */
-    FR_MKFS_ABORTED,        /* (14) The f_mkfs() aborted due to any parameter error */
-    FR_TIMEOUT,             /* (15) Could not get a grant to access the volume within defined period */
-    FR_LOCKED,              /* (16) The operation is rejected according to the file sharing policy */
-    FR_NOT_ENOUGH_CORE,     /* (17) LFN working buffer could not be allocated */
-    FR_TOO_MANY_OPEN_FILES, /* (18) Number of open files > _FS_SHARE */
-    FR_INVALID_PARAMETER    /* (19) Given parameter is invalid */
+    FR_OK 		= ERROR_OK,			/* (0) Succeeded */
+    FR_DISK_ERR		= ERROR_WRITE_ERROR,    	/* (1) A hard error occurred in the low level disk I/O layer */
+    FR_INT_ERR		= ERROR_FAULT,          	/* (2) Assertion failed */
+    FR_NOT_READY	= ERROR_DRIVE_NOT_READY, 	/* (3) The physical drive cannot work */
+    FR_NO_FILE		= ERROR_FILE_NOT_FOUND,		/* (4) Could not find the file */
+    FR_NO_PATH		= ERROR_DIR_ERROR,		/* (5) Could not find the path */
+    FR_INVALID_NAME	= ERROR_SYNTAX_UNKNOWN,		/* (6) The path name format is invalid */
+    FR_DENIED		= ERROR_NO_PERMISSION,		/* (7) Access denied due to prohibited access or directory full */
+    FR_EXIST		= ERROR_FILE_EXISTS,		/* (8) Access denied due to prohibited access */
+    FR_INVALID_OBJECT	= ERROR_SYNTAX_UNKNOWN,		/* (9) The file/directory object is invalid */
+    FR_WRITE_PROTECTED	= ERROR_WRITE_PROTECT,		/* (10) The physical drive is write protected */
+    FR_INVALID_DRIVE	= ERROR_DRIVE_NOT_READY,	/* (11) The logical drive number is invalid */
+    FR_NOT_ENABLED	= ERROR_DRIVE_NOT_READY,	/* (12) The volume has no work area */
+    FR_NO_FILESYSTEM	= ERROR_DRIVE_NOT_READY,	/* (13) There is no valid FAT volume */
+    FR_MKFS_ABORTED	= ERROR_SYNTAX_UNKNOWN,		/* (14) The f_mkfs() aborted due to any parameter error */
+    FR_TIMEOUT		= ERROR_DRIVE_NOT_READY,	/* (15) Could not get a grant to access the volume within defined period */
+    FR_LOCKED		= ERROR_NO_PERMISSION,		/* (16) The operation is rejected according to the file sharing policy */
+    FR_NOT_ENOUGH_CORE	= ERROR_DISK_FULL,		/* (17) LFN working buffer could not be allocated */
+    FR_TOO_MANY_OPEN_FILES = ERROR_NO_CHANNEL,		/* (18) Number of open files > _FS_SHARE */
+    FR_INVALID_PARAMETER = ERROR_SYNTAX_UNKNOWN		/* (19) Given parameter is invalid */
 } FRESULT;
 
 
