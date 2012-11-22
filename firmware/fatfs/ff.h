f7e0e8df7a10fd47ea65c2ff447063129d45f189
Nils Eilers <nils.eilers@gmx.de>
2012-11-22 18:28:40 +0100

    Get rid of a lot of signedness warnings

--- firmware/fatfs/ff.h
+++ firmware/fatfs/ff.h
@@ -64,7 +64,7 @@ typedef WCHAR TCHAR;
 
 #else                       /* ANSI/OEM string */
 #ifndef _INC_TCHAR
-typedef unsigned char TCHAR;
+typedef char TCHAR;
 #define _T(x) x
 #define _TEXT(x) x
 #endif
