0d93784d04dadd97d4c3c9865b8784cee76df69d
Nils Eilers <nils.eilers@gmx.de>
2013-05-16 09:34:22 +0200

    Add fix fuses script for petSD
    New fuses are compatible with sd2iec and XD-2031

---
+++ firmware/petSD/petSD_fix_fuses.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Fix 2011 petSD fuses
+# 
+# ONLY FOR USE WITH PETSD!
+#
+
+PROGRAMMER=avrispmkii
+CONNECTED_TO=usb
+
+# ------------------------------------------------------------------------
+# Do not edit below this line
+#
+echo ""
+echo ""
+echo ""
+echo "petSD bad fuses fixer"
+echo "====================="
+echo ""
+echo "This tool changes the AVR fuses for compatibility with sd2iec and XD-2031."
+echo ""
+echo "FOR USE ONLY WITH PETSD!"
+echo "APPLYING TO OTHER DEVICES MAY BRICK THEM!"
+echo ""
+read -p "If you are shure, enter 'yes' to continue: " sure
+if [ "$sure" == "yes" ]
+   then
+      avrdude -c $PROGRAMMER -P $CONNECTED_TO -p m1284p -v -y -U lfuse:w:0xf7:m -U hfuse:w:0xd2:m -U efuse:w:0xff:m
+   else
+      echo "Aborted."
+      exit 1
+fi
+echo ""
+echo "***********************************************************************************"
+echo ""
+echo "If the last output was..."
+echo ""
+echo "      avrdude: safemode: lfuse reads as F7"
+echo "      avrdude: safemode: hfuse reads as D2"
+echo "      avrdude: safemode: efuse reads as FF"
+echo "      avrdude: safemode: Fuses OK"
+echo ""
+echo "      avrdude done.  Thank you."
+echo ""
+echo "...you're now ready for XD-2031!"
+echo ""
