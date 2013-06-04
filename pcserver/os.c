a35ae374a10d77c9e2758d3b8ed8f4436a75cfc8
Nils Eilers <nils.eilers@gmx.de>
2013-06-05 00:49:29 +0200

    Fix Redmond's ERRORs

--- pcserver/os.c
+++ pcserver/os.c
@@ -290,15 +290,15 @@ char *os_realpath(const char *path)
         //Convert MS errors into standard errors
         switch (GetLastError())
         {
-          case CBM_ERROR_FILE_NOT_FOUND:
+          case ERROR_FILE_NOT_FOUND:
             errno = ENOENT;
             break;
 
-          case CBM_ERROR_PATH_NOT_FOUND: case CBM_ERROR_INVALID_DRIVE:
+          case ERROR_PATH_NOT_FOUND: case ERROR_INVALID_DRIVE:
             errno = ENOTDIR;
             break;
 
-          case CBM_ERROR_ACCESS_DENIED:
+          case ERROR_ACCESS_DENIED:
             errno = EACCES;
             break;
 
