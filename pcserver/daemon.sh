bc33ed62f1ba4ed775ca74c084e0ca80c647e0eb
Nils Eilers <nils.eilers@gmx.de>
2013-07-08 04:52:31 +0200

    Install daemon with execution rights

--- pcserver/daemon.sh
+++ pcserver/daemon.sh
@@ -2,8 +2,8 @@
 #
 # Create a XD-2031 daemon
 #
-cp fsser.init.d.localized /etc/init.d/fsser
-cp fsser.default /etc/default/fsser
+install -m 755 fsser.init.d.localized /etc/init.d/fsser
+install -m 755 fsser.default /etc/default/fsser
 update-rc.d fsser defaults
 service fsser stop
 service fsser start
