c8e1228755c83bc26e3db0fae418683fc05d344a
Nils Eilers <nils.eilers@gmx.de>
2013-07-23 03:01:29 +0200

    Rework service script
    This commit contains a file that was forgotton on last commit,
    unfortunately pushed too soon to fix the commit.

--- pcserver/fsser.init.d
+++ pcserver/fsser.init.d
@@ -1,5 +1,5 @@
-#! /bin/sh
-# 
+#!/bin/sh
+#
 # Copy this file to /etc/init.d/fsser
 #
 ### BEGIN INIT INFO
@@ -10,25 +10,48 @@
 # Default-Stop:      0 1 6
 # Short-Description: XD-2031 Server
 # Description:       github.com/fachat/XD2031
-#                    
+#
 ### END INIT INFO
 
 # Author: Nils Eilers <nils.eilers@gmx.de>
 
 # Read configuration variable file if it is present
+# DAEMON_ARGS -- what it says
+# RUN_AS_USER -- this user will run the server
+# If it is undefined, it will run with root rights
+#
 [ -r /etc/default/fsser ] && . /etc/default/fsser
 
 case "$1" in
 start)
-echo "Starting XD-2031 Server..."
-PREFIX/BINDIR/fsser -D $DAEMON_ARGS >> /var/log/fsser &
+if pidof fsser > /dev/null ; then
+  echo "Server already running."
+else
+  echo "Starting XD-2031 Server..."
+  PREFIX/BINDIR/fsser -D $DAEMON_ARGS >> /var/log/fsser &
+fi
 ;;
 stop)
-echo "Killing XD-2031 Server..."
-killall fsser
+if pidof fsser > /dev/null ; then
+  echo "Killing XD-2031 Server..."
+  kill `pidof fsser`
+  sleep 1
+else
+  echo "No running server found."
+fi
+;;
+restart)
+echo "Restarting XD-2031 Server..."
+if pidof fsser > /dev/null ; then
+  kill `pidof fsser`
+  sleep 1
+else
+  echo "No running server found, starting now."
+fi
+PREFIX/BINDIR/fsser -D $DAEMON_ARGS >> /var/log/fsser &
 ;;
 *)
-echo "Usage: /etc/init.d/fsser {start|stop}"
+echo "Usage: /etc/init.d/fsser {start|stop|restart}"
 exit 1
 ;;
 esac
