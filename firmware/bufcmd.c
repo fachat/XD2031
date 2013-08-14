98231f2b0e397004d69ded0461b28a7e7597c263
A. Fachat <fachat@web.de>
2013-08-14 23:35:16 +0200

    code cleanup

--- firmware/bufcmd.c
+++ firmware/bufcmd.c
@@ -808,38 +808,6 @@ debug_flush();
 	cmdbuf_t *buffer = NULL;
 
 	switch(txbuf->type) {
-/*
-	case FS_OPEN_RD:
-	case FS_OPEN_WR:
-	case FS_OPEN_RW:
-		// proxy the open call for relative files
-		buffer = cmdbuf_find(channelno);
-		if (buffer != NULL && buffer->real_endpoint != NULL) {
-			cbstat = 0;
-debug_printf("opened file with: rxplen=%d\n", packet_get_capacity(rxbuf)); debug_flush();
-			buffer->real_endpoint->provider->submit_call(buffer->real_endpoint->provdata,
-				channelno, txbuf, rxbuf, cmd_callback);
-
-			cmd_wait_cb();
-			plen = packet_get_contentlen(rxbuf);
-                        uint8_t *buf = packet_get_buffer(rxbuf);
-                        uint16_t reclen = 0;
-debug_printf("opened file to get: plen=%d, buf[0]=%d\n", plen, buf[0]); debug_flush();
-                        // store record length if given
-                        if (plen == 3 && buf[0] == CBM_ERROR_OPEN_REL) {
-                                reclen = (buf[1] & 0xff) | ((buf[2] & 0xff) << 8);
-				// this should always be ok, as we only request valid record lengths
-				// and the provider should only positively ack this when everything
-				// is ok with this record length
-				buffer->recordlen = reclen & 0xff;
-				buffer->buf_recordno = 0;	// not loaded
-				buffer->cur_pos_in_record = 0;	// not loaded
-                                buf[0] = CBM_ERROR_OK;
-				buffer->pflag = 0;
-                        }
-		}
-		break;
-*/
 	case FS_CLOSE:
 		buffer = cmdbuf_find(channelno);
 		if (buffer != NULL && buffer->real_endpoint != NULL) {
@@ -1093,26 +1061,6 @@ endpoint_t *bufcmd_provider(void) {
 	return &block_endpoint;
 }
 
-/*
-// proxies a relative file through the bufcmd layer
-int8_t bufcmd_open_relative(endpoint_t **ep, uint8_t channel_no, uint16_t reclen) {
-
-	int8_t err = CBM_ERROR_NO_CHANNEL;
-
-	if (reclen > 254) {
-		return CBM_ERROR_OVERFLOW_IN_RECORD;
-	}
-
-	cmdbuf_t *buffer = cmdbuf_reserve(channel_no);
-	if (buffer != NULL) {
-		buffer->real_endpoint = *ep;
-		buffer->recordlen = reclen;
-		*ep = &relfile_endpoint;
-		err = CBM_ERROR_OK;
-	}
-	return err;
-}
-*/
 
 // wraps the opened channel on the original real_endpoint through the
 // relative file provider, when an "CBM_ERROR_OPEN_REL" is received from the 
