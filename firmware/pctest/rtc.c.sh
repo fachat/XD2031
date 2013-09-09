c40efc596835996c44fb1fa3f5cafb9169108ac9
nils-eilers <nils.eilers@gmx.de>
2013-09-09 02:18:19 +0200

    Move rtc.c.sh to pctest/

--- firmware/pctest/rtc.c.sh
+++ firmware/pctest/rtc.c.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# en.wikipedia.org/wiki/Here_document
+# http://en.wikipedia.org/wiki/Here_document
 
 TESTFILE=rtc
 CFLAGS="-Wall -std=c99 -DHAS_RTC"
-INCLUDE="-I ."
+INCLUDE="-I. -I.. -I../rtc"
 
-gcc -D PCTEST $INCLUDE $CFLAGS $TESTFILE.c -o $TESTFILE || exit 1
+gcc -D PCTEST $INCLUDE $CFLAGS ../rtc/$TESTFILE.c -o $TESTFILE || exit 1
 
 ./$TESTFILE << "EOF"
 # Calling without parameters should output date and time (empty line)
