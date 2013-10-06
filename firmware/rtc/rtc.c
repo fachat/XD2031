a172ba5afc758761b524b6ed1de199fa4841837c
nils-eilers <nils.eilers@gmx.de>
2013-10-06 16:07:06 +0200

    Add some copyleft headers

--- firmware/rtc/rtc.c
+++ firmware/rtc/rtc.c
@@ -1,3 +1,25 @@
+/**************************************************************************
+
+    XD-2031 - Serial line filesystem server for CBMs
+    Copyright (C) 2013 Andre Fachat, Nils Eilers
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+    MA  02110-1301, USA.
+
+***************************************************************************/
+
 // This file holds routines common for all types of
 // real time clocks, e.g. the time parser
 
