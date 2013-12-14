ceb1b0e086c453d388cd4606cbe94b1fe355e030
nils-eilers <nils.eilers@gmx.de>
2013-12-14 09:22:48 +0100

    Fix broken firmware compile (AVR: util/delay.h)

--- firmware/avr/delayhw.h
+++ firmware/avr/delayhw.h
@@ -29,6 +29,7 @@
 #define DELAYHW_H
 
 #include "config.h"         // F_CPU
+#define __DELAY_BACKWARD_COMPATIBLE__
 #include <util/delay.h>
 
 // note that using the static inline versions below breaks the timing!
