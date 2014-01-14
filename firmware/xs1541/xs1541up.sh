9d8130e09cf1fe3be333a54bf64285116b5a19a2
nils-eilers <nils.eilers@gmx.de>
2014-01-14 05:17:26 +0100

    Fix XS-1541 / 3.1541 firmware update scripts

--- firmware/xs1541/xs1541up.sh
+++ firmware/xs1541/xs1541up.sh
@@ -7,6 +7,19 @@ PORT=/dev/ttyUSB0
 # Do not edit below this line
 #
 
+default=XD2031-xs1541.hex
+if test ! -s $default
+then
+  if [ `ls *.hex | wc -w` = "1" ] ; then
+    default=`find . -name *.hex`
+    echo Defaulting to $default
+  else
+    printf "Error: None or more than a single .hex files found.\n"
+    printf "Please specify one with $0 <name.hex>\n"
+    exit 1
+  fi
+fi
+
 # If the Server is running, terminate it first to free the serial line
 if pidof fsser > /dev/null ; then
   sudo service fsser stop
@@ -15,12 +28,10 @@ else
   RESTART=false
 fi
 
-default=XD2031-xs1541.hex
 if [ $# -ne 1 ]
 then
-  if test ! -s $default
-  then
-    echo Could not find default file $default
+  if test ! -s $default ; then
+    echo $default not found
   else
     avrdude -v -c avr109 -P $PORT -b 115200 -p m644 -u -U flash:w:$default
   fi
