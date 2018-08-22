4b62794da8be6315d9bb965ea4aab9cb1479850f
Andre Fachat <fachat@web.de>
2018-08-22 22:20:08 +0200

    implement xdcmd put

--- cmds/put.c
+++ cmds/put.c
@@ -19,92 +19,137 @@
 
 ****************************************************************************/
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 #include "os.h"
 #include "mem.h"
 #include "terminal.h"
 #include "wireformat.h"
 #include "name.h"
 #include "xdcmd.h"
+#include "cerrno.h"
 
+
+#define MAX_BUFFER_SIZE                 64
 
 // put/save command code
 
 
 static int cmd_put_int(int sockfd, int type, int argc, const char *argv[]) {
 
-	uint8_t *name = mem_alloc_c(256, "parse buffer");
-	uint8_t *buf = mem_alloc_c(256, "msg buffer");
-	uint8_t pkgfd = 0;
+	// TODO: unify with get
+        int force = 0;
+        int rv = CBM_ERROR_OK;
 
+        int p = 0;
+        // parse options
+        while ((p < argc) && (argv[p][0] == '-')) {
+                switch(argv[p][1]) {
+                case 'f':
+                        force = 1;
+                        break;
+                case '-':
+                        // break options
+                        p++;
+                        goto endopts;
+                default:
+                        log_error("Unknown option '%c'\n", argv[p][1]);
+                        return CBM_ERROR_SYNTAX_INVAL;
+                }
+                p++;
+        }
+endopts:
+
+        // check filenames
+        if ((argc - p) < 2) {
+                log_error("Too few parameters!\n");
+                return CBM_ERROR_SYNTAX_NONAME;
+        }
+
+        // take target name out of list
+        argc--;
+        // open target on host filesystem
+        const char *trgname = argv[argc];
+
+	// look at the target file
+
+	const int BUFLEN = 255;	
+	uint8_t *name = mem_alloc_c(BUFLEN+1, "parse buffer");
+	uint8_t *buf = mem_alloc_c(BUFLEN+1, "msg buffer");
+	uint8_t pkgfd = 0;
 	nameinfo_t ninfo;
 
-	if (argc > 2) {
-		log_error("Too many parameters!\n");
-		mem_free(name);
-		return -1;
-	}
+        // note that parse_filename parses in-place, nameinfo then points to name buffer
+        strncpy((char*)name, trgname, BUFLEN);
+        name[BUFLEN] = 0;
 
-	if (argc == 1) {
-		// note that parse_filename parses in-place, nameinfo then points to name buffer
-		strncpy((char*)name, argv[0], 255);
-		name[255] = 0;
-	} else {
-		name[0] = 0;
-	}
-	parse_filename(name, strlen((const char*)name), 255, &ninfo, PARSEHINT_LOAD);
+        parse_filename(name, strlen((const char*)name), BUFLEN, &ninfo, PARSEHINT_LOAD);
 
-	int rv = send_longcmd(sockfd, FS_OPEN_WR, pkgfd, &ninfo);
+        if (send_longcmd(sockfd, force ? FS_OPEN_OW : FS_OPEN_WR, pkgfd, &ninfo)) {
+	        if ((recv_packet(sockfd, buf, 256) > 0)
+                	&& buf[FSP_CMD] == FS_REPLY) {
+		       	
+			if (buf[FSP_DATA] == CBM_ERROR_OK) {
 
-	if (rv >= 0) {
-		rv = recv_packet(sockfd, buf, 256);
+		            for (; (rv == CBM_ERROR_OK) && (p < argc); p++) {
+				
+				int infd;
 
-		if (buf[FSP_CMD] == FS_REPLY && buf[FSP_DATA] == CBM_ERROR_OK) {
+				const char *srcname = argv[p];
+				infd = open(srcname, O_RDONLY);
+				if (infd < 0) {
+					rv = errno_to_error(errno);
+					log_errno("Error opening file '%s'\n", srcname);
+					break;
+				}
 
-#if 0
-		    // receive data packets until EOF
-		    do {
-			rv = send_cmd(sockfd, FS_READ, pkgfd);
-			if (rv < 0) {
-				log_errno("Unable to send read request!\n");
-				break;
+                                // receive data packets until EOF
+				ssize_t n = 0;
+                                do {
+                                        if ((n = read(infd, buf + FSP_DATA, MAX_BUFFER_SIZE-FSP_DATA)) < 0) {
+                                                rv = errno_to_error(errno);
+                                                log_errno("Error reading from file!\n");
+                                                break;
+                                        }
+
+					// prepare send buffer
+					buf[FSP_LEN] = FSP_DATA + n;
+					buf[FSP_CMD] = (n == 0) ? FS_WRITE_EOF : FS_WRITE;
+					buf[FSP_FD] = pkgfd;
+
+					if (send_packet(sockfd, buf, buf[FSP_LEN]) < 0) {
+                                                rv = errno_to_error(errno);
+                                                log_errno("Unable to send read request!\n");
+                                                break;
+                                        }
+
+                                        if (recv_packet(sockfd, buf, BUFLEN+1) < 0) {
+                                                rv = errno_to_error(errno);
+                                                log_errno("Could not receive packet!\n");
+                                                break;
+                                        }
+                                        if (buf[FSP_CMD] != FS_REPLY) {
+                                                log_error("Received unexpected packet of type %d!\n", buf[FSP_CMD]);
+                                                rv = CBM_ERROR_FAULT;
+                                                break;
+                                        }
+				} while (n > 0);
+
+			        if (close(infd) < 0) {
+			                log_errno("Could not close source file!");
+			        }
+			    }
+			} else {
+				log_error("Error opening file: %d\n", buf[FSP_DATA]);
 			}
-
-			rv = recv_packet(sockfd, buf, 256);
-
-			if (rv < 0) {
-				log_errno("Could not receive packet!\n");
-				break;
-			}
-			if (buf[FSP_CMD] != FS_DATA && buf[FSP_CMD] != FS_DATA_EOF) {
-				log_error("Received unexpected packet of type %d!\n", buf[FSP_CMD]);
-				break;
-			}
-
-			rv = parse_dir_packet(buf + FSP_DATA, buf[FSP_LEN] - FSP_DATA, &dir);
-
-			if (rv < 0) {
-				break;
-			}
-
-			switch (type) {
-			case 2:
-				print_long_packet(&dir);
-				break;
-			case 1:
-				print_ls_packet(&dir);
-				break;
-			default:
-				print_dir_packet(&dir);
-				break;
-			}
-			
-			if (buf[FSP_CMD] == FS_DATA_EOF) {
-				break;
-			}
-		    } while(rv == 0); 
-#endif
-		    rv = send_cmd(sockfd, FS_CLOSE, pkgfd);
+		} else {
+			log_error("Problem receiving reply to open\n");
 		}
+	} else {
+		log_error("Problem sending open\n");
 	}
 	mem_free(buf);
 	mem_free(name);
